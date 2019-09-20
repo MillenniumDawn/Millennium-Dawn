e54d1acc83d02f82636477a0c3d88d207de1aa2b
Hiddengearz <truemikesmith@gmail.com>
2019-09-19 21:18:13 -0400

    wip

--- tools/validator 2/LogAnalyzer.py
+++ tools/validator 2/LogAnalyzer.py
@@ -5,15 +5,15 @@ class Logs:
         self.scriptDir = os.path.dirname(logFolder)
         self.rootDir = os.path.dirname(os.path.dirname(self.scriptDir))
 
-        self.typeToIgnore = ['lexer.cpp', 'convoys.cpp', 'session.cpp']
+        self.typeToIgnore = ['lexer.cpp', 'convoys.cpp', 'session.cpp', 'gameidler.cpp']
         self.highPriority = []
         self.mediumPriority = []
         self.lowPriority = []
         self.logTypes = {"map.cpp": self.Map, "technologytemplate.cpp": self.TechnologyTemplate, "pdx_entity.cpp": self.PdxEntity,
                          "effect.cpp": self.Effect, "effectimplementation.cpp": self.EffectImplementation, "containerwindow.cpp": self.ContainerWindow,
                          "gamestate.cpp": self.GameState, "graphics.cpp": self.Graphics, "effectbase.cpp": self.EffectBase,
-                         "session.cpp": self.Session, "gfxairplanes.cpp": self.GfxAirplanes,
-                         "triggerimplementation.cpp": self.TriggerImplementation }
+                         "gfxairplanes.cpp": self.GfxAirplanes, "triggerimplementation.cpp": self.TriggerImplementation,
+                         "texturehandler.cpp":self.TextureHandler}
         self.AnalyzeLogs()
 
         print("done")
@@ -25,7 +25,7 @@ class Logs:
             file = os.path.join(self.scriptDir + '/error.log')
             with open(file, 'r', encoding='utf-8', errors='ignore') as file:
                 content = file.readlines()
-                for line in content:
+                for x, line in enumerate(content):
                     if line.startswith('['):
                         ignore = False
                         for logType in self.typeToIgnore:
@@ -35,49 +35,60 @@ class Logs:
                             found = False
                             for logType in self.logTypes:
                                 if logType in line:
+                                    for y in range (x + 1, len(content)):
+                                        if not content[y].startswith('['):
+                                            line = line.rstrip() + '' + content[y]
+                                        else:
+                                            break
                                     functionToCall = self.logTypes[logType]
                                     functionToCall(line)
                                     found = True
                                     break
                             if found == False:
-                                print("Error - Please send this line to the developer: {0}".format(line))
-                    else:
-                        print("need to append last log zzzz")
+                                print("Error: Couldn't catagorize this line, Please send this line to the developer: {0}".format(line))
+                    #else:
+                    #    print("need to append last log zzzz")
+                     #   print(line)
                 input()
 
     def Map(self, line):
-        print("MAP")
+        self.highPriority.append(line)
 
     def TechnologyTemplate(self, line):
-        print("TECH TEMPLATE")
+        if 'has more than 1 XOR, this is not a big problem' in line or 'is primary mutual exclusive ( XOR ) with' in line:
+            self.lowPriority.append(line)
+        else:
+            self.highPriority.append(line)
 
     def PdxEntity(self, line):
-        print("PDX ENTITY")
+        self.lowPriority.append(line)
 
     def Effect(self, line):
-        print("EFFECT")
+        self.highPriority.append(line)
 
     def EffectImplementation(self, line):
-        print("EFFECT IMPLEMENTATION")
+        self.highPriority.append(line)
 
     def ContainerWindow(self, line):
-        print("CONTAINER WINDOW")
+        self.mediumPriority.append(line)
 
     def GameState(self, line):
-        print("GAME STATE")
+        self.lowPriority.append(line)
 
     def Graphics(self, line):
-        print("GRAPHICS")
+        self.highPriority.append(line)
 
     def EffectBase(self, line):
-        print("EFFECT BASE")
-
-    def Session(self, line):
-        print("SESSION")
+        self.mediumPriority.append(line)
 
     def GfxAirplanes(self, line):
-        print("GFX Airplanes")
+        self.mediumPriority.append(line)
 
     def TriggerImplementation(self, line):
-        print("TRIGGER IMPLEMENTATION")
+        self.highPriority.append(line)
 
+    def TextureHandler(self, line):
+        self.highPriority.append(line)
+
+    def CompareLogs(self, newHigh, newMediun, newLow):
+        print()
